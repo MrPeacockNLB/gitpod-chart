bd75432292cf2d5e623b36421398c137fd286b81
Pfau, Sascha <sascha_a.pfau@nordlb.de>
2022-10-20 19:41:38 +0000

    apply first set of fixes to YAMLs

--- prepare.sh
+++ prepare.sh
@@ -12,3 +12,32 @@ CFG=tmp/config.yaml
 
 # write default config file
 gitpod-installer config init -c $CFG --overwrite
+
+#
+# apply fixes
+#
+yq e -i '.domain = "'"${DOMAIN}"'"' $CFG
+yq e -i '.certificate.name = "https-certificates"' $CFG
+
+yq e -i '.observability.logLevel = "trace"' $CFG
+yq e -i '.workspace.runtime.containerdSocket = "/run/k3s/containerd/containerd.sock"' $CFG
+yq e -i '.workspace.runtime.containerdRuntimeDir = "/var/lib/rancher/k3s/agent/containerd/io.containerd.runtime.v2.task/k8s.io/"' $CFG
+yq e -i '.workspace.pvc.size = "10Gi"' $CFG
+yq e -i '.workspace.resources.requests.memory = "500Mi"' $CFG
+yq e -i '.workspace.resources.requests.cpu = "500m"' $CFG
+yq e -i '.workspace.timeoutDefault = "720m"' $CFG
+
+#
+# Enable SSH Gateway
+# ------------------
+#
+# Generate new key without passphrase (-N "")
+#
+rm -rf host.key
+ssh-keygen -t rsa -q -N "" -f tmp/host.key
+
+# create secret yaml
+kubectl create secret generic ssh-gateway-host-key -n $NAMESPACE --from-file=tmp/host.key --dry-run=client --output=yaml > manifests/ssh-gateway-host-key.yaml
+
+# append configuration settings for SSH gateway
+yq e -i '.sshGatewayHostKey.kind = "secret" | .sshGatewayHostKey.name = "ssh-gateway-host-key"' $CFG
