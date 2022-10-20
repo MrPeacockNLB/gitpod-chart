88b1bf590a2305d5deef2e625346ff7b1ac9b1d1
Pfau, Sascha <sascha_a.pfau@nordlb.de>
2022-10-20 19:17:25 +0000

    first step of prepare

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                                                                                                                                                                                ▲
+
+# read global configuration
+source config
+
+# write default config file
+gitpod-installer config init -c config.yaml --overwrite
